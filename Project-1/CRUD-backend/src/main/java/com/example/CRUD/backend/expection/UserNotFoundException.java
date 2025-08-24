8ffbe5988c75d5549db8bea6be53a10859203f8f
aryan-gaurv <161956388+aryan-gaurv@users.noreply.github.com>
2025-08-25 01:26:16 +0530

    Delete Project-1 directory

--- Project-1/CRUD-backend/src/main/java/com/example/CRUD/backend/expection/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.CRUD.backend.expection;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException (Long id){
-        super("Could Not found the user "+id);
-    }
-}
--- Project-1/CRUD-backend/src/main/java/com/example/CRUD/backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.CRUD.backend.repository;
-
-import com.example.CRUD.backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
